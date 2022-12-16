788487acd6c983f79359d3b4b62024e09bfb7d25
Suraj Pun <s_pun@outlook.com>
2022-12-16 01:58:08 +0000

    Added dropdown menu 2

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function handleClick() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function handleClick2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
@@ -11,6 +15,17 @@ window.onclick = function (event) {
       const openDropdown = dropdowns[i];
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
+      }
+    }
+
+    if (!event.target.matches(".dropbtn2")) {
+      const dropdowns = document.getElementsByClassName("dropdown-content2");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
       }
     }
   }
